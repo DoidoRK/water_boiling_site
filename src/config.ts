3f018114b8ac26e43141444cf3eb9463e8324efd
DoidoRK <doidobr46@gmail.com>
2024-07-04 12:44:37 -0300

    Separating logic component from page component for better project organization.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const WS_PORT = 8081;
 const SERVER_ADDRESS = "192.168.15.5"
 
-const webSocketAddress = `ws://${SERVER_ADDRESS}:${WS_PORT}`
+const serverAddress = `ws://${SERVER_ADDRESS}:${WS_PORT}`
 
-export { webSocketAddress };
+export { serverAddress };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Typography, CircularProgress } from '@mui/material';
-import { webSocketAddress } from '../config';
+import useWebSocket from '../services/webSocketService';
 
 const Home: React.FC = () => {
-  const [loading, setLoading] = useState(true);
-  const [connected, setConnected] = useState(false);
-
-  useEffect(() => {
-    const socket = new WebSocket(webSocketAddress);
-
-    socket.onopen = () => {
-      console.log('Connected to WebSocket server');
-      setLoading(false);
-      setConnected(true);
-    };
-
-    socket.onclose = () => {
-      console.log('Disconnected from WebSocket server');
-      setLoading(false);
-      setConnected(false);
-    };
-
-    socket.onmessage = (message) => {
-      console.log(message)
-    }
-
-    return () => {
-      socket.close();
-    };
-  }, []);
+  const { loading, connected } = useWebSocket();
 
   if (loading) {
     return <CircularProgress />;
